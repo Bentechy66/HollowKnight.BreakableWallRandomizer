f7f91732331fcd8ab8b70216b5492cdc26530da7
ben <sendbenspam@yahoo.co.uk>
2023-03-03 23:13:23 +0000

    a

--- ModClass.cs
+++ ModClass.cs
@@ -20,7 +20,7 @@ namespace BreakableWallRandomiser
         public static WallRandoSettings settings = new WallRandoSettings();
 
         new public string GetName() => "Breakable Wall Randomizer";
-        public override string GetVersion() => "v2_RC2";
+        public override string GetVersion() => "1.0.0.0";
         public override void Initialize()
         {
             Log("Initializing...");
